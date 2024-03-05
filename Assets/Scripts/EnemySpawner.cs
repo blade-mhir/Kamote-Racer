a00a6b39b2cd1678f67be97708480c23fe988cfc
blade-mhir <bferrer@gbox.adnu.edu.ph>
2024-03-05 20:12:52 +0800

    dunno

--- Assets/Scripts/EnemySpawner.cs
+++ Assets/Scripts/EnemySpawner.cs
@@ -4,31 +4,30 @@ using UnityEngine;
 
 public class EnemySpawner : MonoBehaviour
 {
-
     public GameObject[] cars;
-    int carNum;
-    public float maxPos = 4f;
+    public float[] lanePositions = { -4.2f, -1.45f, 1.45f, 4.2f }; // Define your lane positions
     public float delayTimer = 0.5f;
     float timer;
-
-
 
     // Start is called before the first frame update
     void Start()
     {
-       timer = delayTimer;
+        timer = delayTimer;
     }
 
     // Update is called once per frame
     void Update()
     {
         timer -= Time.deltaTime;
-        if (timer <=0) {
-                Vector3 enemyPos = new Vector3(Random.Range(-4f, 4f), transform.position.y, transform.position.z);
-                carNum = Random.Range (0, 20);
-                Instantiate (cars[carNum], enemyPos, transform.rotation);
-                timer = delayTimer;
+        if (timer <= 0)
+        {
+            int laneIndex = Random.Range(0, 11); // Randomly select a lane
+            Vector3 enemyPos = new Vector3(lanePositions[laneIndex], transform.position.y, transform.position.z);
+
+            int carNum = Random.Range(0, 20); // Adjust as needed
+            Instantiate(cars[carNum], enemyPos, transform.rotation);
+
+            timer = delayTimer;
         }
-         
     }
 }
